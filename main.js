53ffee7aa6126fb1df3eaffea4690be0277adab5
Ryu <cozy6292@gmail.com>
2020-01-09 07:47:49 +0000

    Node HTTP sever making

---
+++ main.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 3000;
+http.createServer(function(request, response) {
+console.log('요청 수신.');
+response.writeHead(200, {'Content-Type': 'text/html'});
+response.write('Hello Git!');
+response.end();
+
+}).listen(port);
+console.log(`서버 시작 - 포트:${port}`);
